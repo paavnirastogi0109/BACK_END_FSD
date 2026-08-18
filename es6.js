32c636f68ea2f3b637d1dc861e92983f9f7833b5
Paavni Rastogi <paavni.ras07@gmail.com>
2026-08-18 09:36:52 +0530

    es6 file added

---
+++ es6.js
@@ -0,0 +1,18 @@
+//javascript
+//function black of code
+//function fname()
+//function calling()
+
+
+
+function sayHello(a,b){
+console.log("cse 24 fsd");
+return a+b;
+}
+console.log(sayHello(3,2));
+
+//arrow function(no arguments = oneliner code)
+const sayhello=()=>{
+    console.log("arrowfunction")
+}
+sayhello();
