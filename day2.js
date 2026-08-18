85c4052113170d81856fa111e52ed613f7da5bc0
Paavni Rastogi <paavni.ras07@gmail.com>
2026-08-18 09:25:26 +0530

    day2 js file added

---
+++ day2.js
@@ -0,0 +1,11 @@
+//synchronous and  asynchronous
+console.log("task3")
+function hello(){
+    console.log("task1");
+}
+hello();
+console.log("task2");
+
+function hello(){
+    console.log("task1")
+}
